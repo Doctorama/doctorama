ef700441372c5c6766201acd2a37b99d6a89ad47
maugerea <augereau.mickael@gmail.com>
2014-12-08 14:07:45 +0100

    Modification vue liste doctorant, fichier comportement.js

--- web/bundles/doctorama/public/js/comportement.js
+++ web/bundles/doctorama/public/js/comportement.js
@@ -1,5 +1,20 @@
-$("#plus_enc").css("cursor","pointer");
+$(document).ready(function(){
 
-$("#plus_enc").click(function(){
-	$(this).before('<input type="text" class="form-control" name="enc_th[]" style="margin-top:15px;" placeholder="Encadrant de la thèse">');
+	// Ajout des champs encadrants dans la création d'un dossier
+	$("#plus_enc").css("cursor","pointer");
+	$("#plus_enc").click(function(){
+		$(this).before('<input type="text" class="form-control" name="enc_th[]" style="margin-top:15px;" placeholder="Encadrant de la thèse">');
+	});
+
+	// Gestion du type d'affichage lors des listes des doctorants
+	$('.vue-liste-doctorant').hide();
+	$('#btn_grille').click(function(){
+		$('.vue-liste-doctorant').hide();
+		$('.vue-grille-doctorant').show();
+	});
+	$('#btn_list').click(function(){
+		$('.vue-grille-doctorant').hide();
+		$('.vue-liste-doctorant').show();
+	});
+
 });
