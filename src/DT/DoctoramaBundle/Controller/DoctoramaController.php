efc25ef979206283058e207a35b870827335b2d2
ahmadimhd <ahmadi.mehdi3@gmail.com>
2014-12-10 08:34:20 +0100

    Ajout d'une vue importation de fichier CSV, modification de la page Login, Ajout d'un service de lecture de fichier CSV

--- src/DT/DoctoramaBundle/Controller/DoctoramaController.php
+++ src/DT/DoctoramaBundle/Controller/DoctoramaController.php
@@ -15,6 +15,22 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class DoctoramaController extends Controller {
     
+    public function import_csvAction($file)
+    {
+    	$ligne = 2; // compteur de ligne
+		$fic = fopen($file, "a+");
+		while($tab=fgetcsv($fic,1024,';'))
+		{
+			$champs = count($tab);//nombre de champ dans la ligne en question	
+			echo "<b> Les " . $champs . " champs de la ligne " . $ligne . " sont :</b><br />";
+			$ligne ++;
+			//affichage de chaque champ de la ligne en question
+			for($i=0; $i<$champs; $i ++)
+			{
+				echo $tab[$i] . "<br />";
+			}
+		}
+    }
     
     public function mesDoctorantsAction(Request $request)
     {
@@ -51,17 +67,20 @@ class DoctoramaController extends Controller {
         return $this->render('DTDoctoramaBundle:Doctorama:admin_dossier.html.twig', array('title' => 'Dossier de suivis'));
     }
     
-    
     public function creerDossierSuivisAction(Request $request)
     {
         return $this->render('DTDoctoramaBundle:Doctorama:creer_dossier.html.twig', array('title' => 'Créer dossier de suivis'));
     }
-    
     
     public function modifDossierSuivisAction($id_doctorant, Request $request)
     {
         
         //il faudra charger les infos du doctorant concerné.
         return $this->render('DTDoctoramaBundle:Doctorama:modif_dossier.html.twig', array('title' => 'Modifier dossier de suivis'));
+    }
+    
+    public function importCsvAction(Request $request)
+    {
+        return $this->render('DTDoctoramaBundle:Doctorama:import_csv.html.twig', array('title' => 'Importation fichier CSV'));
     }
 }
--- src/DT/DoctoramaBundle/Services/ReunionService.php
+++ src/DT/DoctoramaBundle/Services/ReunionService.php
@@ -10,18 +10,45 @@ class ReunionService
 		$reunion = new Reunion();
 		$reunion->setLieu($lieu);
 		$reunion->setDate($date);
-		
 		$reunion->setPersonnes($personnes);
-		//ou
-		/*
-		foreach($personnes as $personne)
-		{
-			$reunion->addPersonne($personne);
-		}
-		*/
-		
+
 		$em = $this->getDoctrine()->getManager();
 		$em->persist($product);
 		$em->flush();
+	}
+}
+
+
+//Importation fichier CSV service
+class CSV_service
+{
+	public function ImportData($file)
+	{
+		$ligne = 1; //compteur de ligne
+		$fic = fopen($file, "a+");
+		while($tab=fgetcsv($fic,1024,';'))
+		{
+			$champs = count($tab); //nombre de champ dans la ligne en question	
+			if ($ligne>1) //affichage de chaque champ de la ligne en question
+			{
+				for($i=0; $i<$champs; $i ++)
+				{
+					echo $tab[$i] . "<br />";
+				}
+			}
+			$ligne ++;
+		}
+		
+		/*
+		$reunion = new Reunion();
+		$reunion->setLieu($lieu);
+		$reunion->setDate($date);
+		
+		$reunion->setPersonnes($personnes);
+
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($product);
+		$em->flush();
+		*/
 	}
 }
