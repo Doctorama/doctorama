6e336ec3787105408c5b20034c6c68926d5bc7f5
Bino <ivance.kevin@gmail.com>
2014-12-10 17:18:56 +0100

    Mise à jour de la Fixtures LoadQuestionReponse : Ajout de deux réponses

--- src/DT/DoctoramaBundle/DataFixtures/loadQuestionReponse.php
+++ src/DT/DoctoramaBundle/DataFixtures/loadQuestionReponse.php
@@ -21,14 +21,19 @@ class loadQuestionReponse implements FixtureInterface{
 
     	// Création des deux réponses
 		$reponse = new Reponse;
+		$reponse1 = new Reponse;
 
 		// La résponse sera "4"
 		$reponse->setReponse('4');
+		// Non la réponse c'est blanc !
+		$reponse1->setReponse("Non c'est blanc !");
 
 		// Elle sera associé à la question avec l'id= 1 
 		$reponse->setQuestion($question);
+		$reponse1->setQuestion($question);
 
 		$manager->persist($reponse);			
+		$manager->persist($reponse1);	
 		$manager->flush();
 		
 	}
