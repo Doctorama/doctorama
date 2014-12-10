e82229c98573db037202ab4093bcc25df6dafc85
maugerea <augereau.mickael@gmail.com>
2014-12-10 15:22:44 +0100

    fichier js modifié

--- web/bundles/doctorama/public/js/comportement.js
+++ web/bundles/doctorama/public/js/comportement.js
@@ -16,5 +16,44 @@ $(document).ready(function(){
 		$('.vue-grille-doctorant').hide();
 		$('.vue-liste-doctorant').show();
 	});
+	
+	//Pagination
+	$(".btn_nb_par_page").click(function(){
+		var nb_visible = this.value;
+		var nb_doctorant = $("tr").length - 1;
+		var nb_page = Math.ceil(nb_doctorant/nb_visible);
+		
+		$('.active').removeClass("active btn-info");
+		$(this).addClass("active btn-info");
+		
+		$("article:nth-child(n+"+nb_visible+")").hide();
+		$("article:nth-child(-n+"+nb_visible+")").show();
+		
+		$("tr:nth-child(n+"+nb_visible+")").hide()
+		$("tr:nth-child(-n+"+nb_visible+")").show();
+		
+		var html = '<li><a href="#"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a></li>';
+		for(var i = 1; i <=nb_page; i++)
+		{
+			html+=' <li><a href="#" class="page">'+i+'</a></li>';
+		}
+		html+='<li><a href="#" ><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>';
+		$('.pagination').html(html);
+	});
+	
+	$(document).on('click','.page',function(){
+		
+		var num_page = this.text;
+		var nb_visible = $('.active.btn-info').html();
+		
+		var deb = (num_page-1)*nb_visible + 1;
+		var fin = num_page*nb_visible;
+		
+		$("article").hide();
+		$("article:nth-child(n+"+deb+"):nth-child(-n+"+fin+")").show();
+		
+		$("tr").hide();
+		$("tr:nth-child(n+"+deb+"):nth-child(-n+"+fin+")").show();
+	});
 
 });
