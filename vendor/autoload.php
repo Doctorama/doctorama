c8d7ca82da8d148d7a4cde047aac7b61553b0c37
ahmadimhd <ahmadi.mehdi3@gmail.com>
2014-12-11 09:23:47 +0100

    Suppression de la fonctionnalité CSV

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit2764c0080b5e27f307203e8b7f313405::getLoader();
+return ComposerAutoloaderInit4ffc62da303b7925e0ad2bd2e778df3f::getLoader();
