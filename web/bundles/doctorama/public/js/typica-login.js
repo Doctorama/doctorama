7267343ce99b20c0032c5b596724d5199c0bc6ae
ahmadimhd <ahmadi.mehdi3@gmail.com>
2014-12-12 13:36:29 +0100

    Changement page 'authentification

---
+++ web/bundles/doctorama/public/js/typica-login.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+	$.backstretch([
+      "http://www.univ-larochelle.fr/IMG/rubon77.jpg?1326297725", 
+      "http://www.univ-larochelle.fr/IMG/rubon3.jpg"
+  	], {duration: 2000, fade: 750});
+		
+});
+
+
