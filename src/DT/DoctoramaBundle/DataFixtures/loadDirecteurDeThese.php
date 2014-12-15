aba508eb8e2d1ea46063334ca3c39c64a60e601f
pdurancy <paul.durancy@gmail.com>
2014-12-15 15:11:28 +0100

    Creation directeur de these et ajout de sa these

---
+++ src/DT/DoctoramaBundle/DataFixtures/loadDirecteurDeThese.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace DT\DoctoramaBundle\DataFixtures;
+
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use DT\DoctoramaBundle\Entity\These;
+use DT\DoctoramaBundle\Entity\Encadrant;
+use DT\SecurityBundle\Entity\Compte;
+
+class loadDirecteurDeThese extends AbstractFixture implements OrderedFixtureInterface{
+	public function load(ObjectManager $manager){
+		$directeur = new Encadrant;
+		$directeur->setNom('Toto');
+		$directeur->setPrenom('Jean');
+		$directeur->setNomUsage('Toto');
+		$directeur->setCivilite('Monsieur');
+		$directeur->setAdresse('1 rue des roses');
+		$directeur->setMail('toto@adresse.com');
+		$directeur->setDateDeNaissance(new \DateTime('2000-01-01'));
+		$directeur->setNationalite('FR');
+		$directeur->setVilleDeNaissance('La Rochelle');
+		$directeur->setPaysDeNaissance('France');
+		$directeur->setDepDeNaissance('17');
+		
+		$compte = new Compte;
+		$compte->setUsername('JToto');
+		$compte->setPlainPassword('5678');
+		$compte->setEnabled(true);
+		$compte->setEmail('toto@adresse.com');
+		$compte->setEncadrant($directeur);
+		
+		$manager->persist($directeur);
+		$manager->persist($compte);
+		$manager->flush();
+		
+		$dir = $manager->getRepository('DTDoctoramaBundle:These')->findByTitreThese('Une These');
+		$these = $dir[sizeof($dir)-1];
+		$these->addDirecteurDeThese($directeur);
+        $manager->persist($these);
+    
+		$manager->flush();
+	}
+	public function getOrder(){
+		return 3;
+	}
+}
