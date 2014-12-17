67c8faeaed4c074782b6d84fb7b94c79dfbd2d53
ElhajjamWail <elhajjamwail@gmail.com>
2014-12-17 13:34:52 +0100

    Ajout de test de sécurité TEST-DOCTORANT et TEST-ENCADRANT

--- src/DT/SecurityBundle/Tests/Controller/DemoControllerTest.php
+++ src/DT/SecurityBundle/Tests/Controller/DemoControllerTest.php
@@ -14,7 +14,7 @@ class DemoControllerTest extends WebTestCase
     {
         $this->client = static::createClient();
     }
-	
+	//TEST_ADMIN
 	public function testSecuredAdmin()
     {
         $this->logInAdmin();
@@ -37,7 +37,7 @@ class DemoControllerTest extends WebTestCase
         $cookie = new Cookie($session->getName(), $session->getId());
         $this->client->getCookieJar()->set($cookie);
     }
-	
+	//TEST_USER
 	public function testSecuredUser()
     {
         $this->logInUser();
@@ -59,4 +59,51 @@ class DemoControllerTest extends WebTestCase
         $cookie = new Cookie($session->getName(), $session->getId());
         $this->client->getCookieJar()->set($cookie);
     }
+	//TEST_DOCTORANT
+	public function testSecuredDoctorant()
+    {
+        $this->logInDoctorant();
+
+        $crawler = $this->client->request('GET', '/encadrant/doctorant_labo/creer_dossier_suivis');
+
+        $this->assertFalse($this->client->getResponse()->isSuccessful());
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("")')->count());
+    }
+	
+	private function logInDoctorant()
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        $firewall = 'secured_area';
+        $token = new UsernamePasswordToken('doctorant', null, $firewall, array('ROLE_DOCTORANT'));
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
+	//TEST_ENCADRANT
+	public function testSecuredEncadrant()
+    {
+        $this->logInEncadrant();
+
+        $crawler = $this->client->request('GET', '/encadrant/doctorant_labo/creer_dossier_suivis');
+
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("")')->count());
+    }
+	
+	private function logInEncadrant()
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        $firewall = 'secured_area';
+        $token = new UsernamePasswordToken('encadrant', null, $firewall, array('ROLE_ENCADRANT'));
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
+
 }
