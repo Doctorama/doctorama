be4e429a8ae6e48930e5e2bb48aa51057f6e5370
pdurancy <paul.durancy@gmail.com>
2014-12-17 10:33:59 +0100

    Ajout de template

--- src/DT/DoctoramaBundle/DataFixtures/loadTemplateDossierDeSuivi.php
+++ src/DT/DoctoramaBundle/DataFixtures/loadTemplateDossierDeSuivi.php
@@ -15,10 +15,14 @@ class loadTemplateDossierDeSuivi extends AbstractFixture implements OrderedFixtu
 		$template->setTitre('Un template');
 		
 		$template2 = new TemplateFicheSuivi;
-		$template2->setTitre('Un template');
+		$template2->setTitre('Un template1');
+		
+		$template3 = new TemplateFicheSuivi;
+		$template3->setTitre('Un template2');
 		
 		$manager->persist($template);
 		$manager->persist($template2);
+		$manager->persist($template3);
 		$manager->flush();
 		
 		$t = $manager->getRepository('DTDoctoramaBundle:DossierDeSuivi')->findById(1);
@@ -26,6 +30,7 @@ class loadTemplateDossierDeSuivi extends AbstractFixture implements OrderedFixtu
 		
 		$template->setDossierDeSuivi($t[0]);
 		$template2->setDossierDeSuivi($t2[0]);
+		$template3->setDossierDeSuivi($t2[0]);
 		
 		$manager->flush();
 	}
