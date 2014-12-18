6fb0ad2a867635011994cdf09ae18eb8e8e64f44
ahmadimhd <ahmadi.mehdi3@gmail.com>
2014-12-18 15:36:15 +0100

    Rajout d'image à la page login

--- web/bundles/doctorama/public/js/typica-login.js
+++ web/bundles/doctorama/public/js/typica-login.js
@@ -1,7 +1,9 @@
 jQuery(document).ready(function($) {
 	$.backstretch([
       "http://www.atelierpositif.com/wp-content/uploads/2012/03/08-copie-copie-800x438.jpg", 
-      "http://etudinfo.com/image/photo/campus-universite-de-5171366c0355c.jpg"
+      "http://etudinfo.com/image/photo/campus-universite-de-5171366c0355c.jpg",
+      "http://www.univ-larochelle.fr/IMG/rubon77.jpg?1326297725",
+      "http://www.univ-larochelle.fr/IMG/rubon3.jpg?1323169238"
   	], {duration: 4000, fade: 750});
 		
 });
